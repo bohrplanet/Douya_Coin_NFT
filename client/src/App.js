70c516f8c57d32357b651a5155a632349304953e
Yinbo <bohrplanet@gmail.com>
2022-03-17 23:29:55 -0700

    add front page element

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import Douya from "./contracts/Douya.json";
 import getWeb3 from "./getWeb3";
-
+import { Layout, Menu, Row, Col, Image } from 'antd';
+import 'antd/dist/antd.css'
 import "./App.css";
+import big_pic from './img/big_pic.png'
+
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -39,33 +44,52 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    // await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    // // Get the value from the contract to prove it worked.
+    // const response = await contract.methods.get().call();
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+    // // Update state with the result.
+    // this.setState({ storageValue: response });
   };
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    // if (!this.state.web3) {
+    //   return <div>Loading Web3, accounts, and contract...</div>;
+    // }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo">Douya</div>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="1">Buy</Menu.Item>
+            <Menu.Item key="2">NFT</Menu.Item>
+            <Menu.Item key="3">Gaming</Menu.Item>
+            <Menu.Item key="4">Staking</Menu.Item>
+            <Menu.Item key="5">About Douya</Menu.Item>
+            <Menu.Item key="6">Connect</Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="content" style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Row>
+              <Col span={12} className="main_left">
+                <h1 style={{ color: "#f36b3b"}}>Douya</h1>
+                <h1 style={{ color: "#f36b3b"}}>MΞmΞ coin</h1>
+              </Col>
+              <Col span={12} className="main_right"> 
+                <Image
+                  src={big_pic}
+                  preview={false}
+                  height={900}
+                  width={900}
+                />
+              </Col>
+            </Row>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Made with ❤ by Bohr @2022 email: crypto18mo@gmail.com</Footer>
+      </Layout>
     );
   }
 }
